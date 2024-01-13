d0a613473f4d21e621f53ef72279f15a068e03d5
RyanFu08 <108840013+RyanFu08@users.noreply.github.com>
2024-01-13 13:18:01 -0800

    Update RMXQ.cpp

--- ds/range/RMXQ.cpp
+++ ds/range/RMXQ.cpp
@@ -21,36 +21,21 @@ not verified
 struct RMXQ {
 	int len;
 	vector<int> segmx;
-	vector<int> segmn;
 	RMXQ() {}
 	RMXQ(int l) {
 		len = 1ll<<(int) (log2(l)+1);
 		segmx = vector<int>(2*len,-INF);
-		segmn = vector<int>(2*len,INF);
 	}
 	void set(int ind, int val) {
 		assert(0 <= ind && ind < len);
-		ind += len;
-		segmx[ind] = val;
-		segmn[ind] = val;
-		while (ind /= 2) {
-			segmx[ind] = max(segmx[2*ind], segmx[2*ind+1]);
-			segmn[ind] = min(segmn[2*ind], segmn[2*ind+1]);
-		}
+		ind += len; segmx[ind] = val;
+		while (ind /= 2) {segmx[ind] = max(segmx[2*ind], segmx[2*ind+1]);}
 	}
 	int rmxq(int l, int r) {
 		int res = -INF; r++;
 		for (l += len, r += len; l < r; l /= 2, r /= 2) {
 			if (l&1) res = max(res, segmx[l++]);
 		    if (r&1) res = max(res, segmx[--r]);
-		}
-		return res;
-	}
-	int rmnq(int l, int r) {
-		int res = INF; r++;
-		for (l += len, r += len; l < r; l /= 2, r /= 2) {
-			if (l&1) res = min(res, segmn[l++]);
-		    if (r&1) res = min(res, segmn[--r]);
 		}
 		return res;
 	}
