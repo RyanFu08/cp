e99e56e2cc5d0784a78ab8a38f62b9a3d0abdc05
RyanFu08 <108840013+RyanFu08@users.noreply.github.com>
2024-01-13 13:27:37 -0800

    Update RMXQ.cpp

--- ds/range/RMXQ.cpp
+++ ds/range/RMXQ.cpp
@@ -15,23 +15,31 @@ using namespace std;
 0 indexed Range Max Query
 - set(ind, val) sets the value at ind to val in O(log n)
 - rmxq(l, r) returns the min in [l,r] in O(log n)
-not verified
+verified
 */
 
-struct RMXQ {
-	int len;
+struct RMQ {
+	int len = 1;
 	vector<int> segmx;
-	RMXQ() {}
-	RMXQ(int l) {
-		len = 1ll<<(int) (log2(l)+1);
+	RMQ() {}
+	RMQ(int l) {
+		while (len < l) len *= 2;
 		segmx = vector<int>(2*len,-INF);
+	}
+	RMQ(vector<int> arr) {
+		while (len < sz(arr)) len *= 2;
+		segmx = vector<int>(2*len,-INF);
+		for (int i = 0; i < sz(arr); i++) {segmx[len+i] = arr[i];}
+		for (int i = len-1; i > 0; i--) {
+			segmx[i] = max(segmx[2*i], segmx[2*i+1]);
+		}
 	}
 	void set(int ind, int val) {
 		assert(0 <= ind && ind < len);
 		ind += len; segmx[ind] = val;
 		while (ind /= 2) {segmx[ind] = max(segmx[2*ind], segmx[2*ind+1]);}
 	}
-	int rmxq(int l, int r) {
+	int rangemax(int l, int r) {
 		int res = -INF; r++;
 		for (l += len, r += len; l < r; l /= 2, r /= 2) {
 			if (l&1) res = max(res, segmx[l++]);
